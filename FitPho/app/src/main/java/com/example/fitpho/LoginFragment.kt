b07df32456f16f81e8cd755f317256407e6fc9f2
yunsuk990 <yunsuk990@naver.com>
2022-09-09 21:30:37 +0900

    Update LoginFragment.kt

--- FitPho/app/src/main/java/com/example/fitpho/LoginFragment.kt
+++ FitPho/app/src/main/java/com/example/fitpho/LoginFragment.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fitpho.databinding.FragmentLoginBinding
+import com.example.fitpho.databinding.FragmentSplashBinding
 
 class LoginFragment : Fragment() {
+
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
