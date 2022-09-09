10e6b8b3f6da9e51436712f159be38e237346551
yunsuk990 <yunsuk990@naver.com>
2022-09-09 17:38:23 +0900

    Update SplashFragment.kt

--- FitPho/app/src/main/java/com/example/fitpho/SplashFragment.kt
+++ FitPho/app/src/main/java/com/example/fitpho/SplashFragment.kt
@@ -5,15 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBar
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.fitpho.databinding.FragmentSplashBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import android.app.ActionBar as ActionBar1
 
 class SplashFragment : Fragment() {
+
+    private var _binding:FragmentSplashBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch(Dispatchers.IO){
+            delay(2000)
+            withContext(Dispatchers.Main){
+                findNavController().navigate(R.id.action_global_loginFragment)
+            }
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
