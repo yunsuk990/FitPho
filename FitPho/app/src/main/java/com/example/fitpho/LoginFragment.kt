22af74995fd86c68d78398c50b6e98ff2df2dffe
yunsuk990 <yunsuk990@naver.com>
2022-09-11 00:06:54 +0900

    Add LoginModel

--- FitPho/app/src/main/java/com/example/fitpho/LoginFragment.kt
+++ FitPho/app/src/main/java/com/example/fitpho/LoginFragment.kt
@@ -1,12 +1,12 @@
 package com.example.fitpho
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.fitpho.databinding.FragmentLoginBinding
-import com.example.fitpho.databinding.FragmentSplashBinding
 
 class LoginFragment : Fragment() {
 
@@ -21,6 +21,19 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val id = binding.userId.text.toString() //사용자 ID
+        val passwd = binding.userPasswd.text.toString() //사용자 Passwd
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        
     }
 
     override fun onDestroyView() {
