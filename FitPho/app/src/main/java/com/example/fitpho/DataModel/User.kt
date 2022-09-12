44b6f591c408d32ed65fdaa36da9100c59e18b2d
yunsuk990 <yunsuk990@naver.com>
2022-09-12 21:46:59 +0900

    Create User.kt

---
+++ FitPho/app/src/main/java/com/example/fitpho/DataModel/User.kt
@@ -0,0 +1,10 @@
+package com.example.fitpho.DataModel
+
+class User {
+    var id: String? = null
+        get() {return field}
+        set(value){ field = value}
+    var passwd: String? = null
+        get() {return field}
+        set(value){ field = value}
+}
