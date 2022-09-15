84c56f8861db9dac73376b437c13f779d77b28c6
yunsuk990 <yunsuk990@naver.com>
2022-09-15 09:13:35 +0900

    Update API.kt

--- FitPho/app/src/main/java/com/example/fitpho/Network/API.kt
+++ FitPho/app/src/main/java/com/example/fitpho/Network/API.kt
@@ -1,13 +1,19 @@
 package com.example.fitpho.Network
 
-import com.example.fitpho.DataModel.User
+import com.example.fitpho.NetworkModel.Login
+import com.example.fitpho.NetworkModel.LoginResponse
+import com.example.fitpho.NetworkModel.Register
+import com.example.fitpho.NetworkModel.RegisterResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface API {
 
-    @POST("android")
-    fun getLoginResponse(@Body user: User): Call<String>
+    @POST("/login")
+    fun signIn(@Body login:Login): Call<LoginResponse>
+    //AuthResponse -> Body로 들어오는 데이터
 
+    @POST("/register")
+    fun registerIn(@Body register: Register): Call<RegisterResponse>
 }
