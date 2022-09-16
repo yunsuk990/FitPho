562afea702a92eeb942f3b5c074a7d80df5f3b6f
yunsuk990 <yunsuk990@naver.com>
2022-09-16 13:30:37 +0900

    Update RetrofitBuilder.kt

--- FitPho/app/src/main/java/com/example/fitpho/NetworkModel/RetrofitBuilder.kt
+++ FitPho/app/src/main/java/com/example/fitpho/NetworkModel/RetrofitBuilder.kt
@@ -1,12 +1,17 @@
 package com.example.fitpho.NetworkModel
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "http://10.0.2.2:8080"
+const val BASE_URL = "http://192.168.10.114:8080"
 fun getRetrofit(): Retrofit {
+    val gson: Gson = GsonBuilder()
+        .setLenient()
+        .create()
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create()).build()
+        .addConverterFactory(GsonConverterFactory.create(gson)).build()
     return retrofit
 }
