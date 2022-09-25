4da145e07cb1f8ec61ba0a764a2a8689b3b4b280
yunsuk990 <yunsuk990@naver.com>
2022-09-26 03:02:37 +0900

    Update GuideFragment.kt

--- FitPho/app/src/main/java/com/example/fitpho/GuideFragment.kt
+++ FitPho/app/src/main/java/com/example/fitpho/GuideFragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.example.fitpho.databinding.FragmentGuideBinding
+import com.example.fitpho.databinding.FragmentSplashBinding
 
 
 class GuideFragment : Fragment() {
+
+
+    private var _binding: FragmentGuideBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guide, container, false)
+
+        _binding = FragmentGuideBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.chest.setOnClickListener{
+            val bundle = bundleOf("Title" to "가슴")
+            findNavController().navigate(R.id.action_guideFragment_to_guideDetailFragment, bundle)
+        }
+
+    }
+
+
 }
