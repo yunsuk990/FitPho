e2317ad8c9492e236bc964dc778e762a3afecb59
yunsuk990 <yunsuk990@naver.com>
2022-09-26 16:46:56 +0900

    Update GuideDetailFragment.kt

--- FitPho/app/src/main/java/com/example/fitpho/GuideDetailFragment.kt
+++ FitPho/app/src/main/java/com/example/fitpho/GuideDetailFragment.kt
@@ -6,27 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.fitpho.databinding.FragmentGuideDetailBinding
-import com.example.fitpho.databinding.FragmentHomeBinding
-
 
 class GuideDetailFragment : Fragment() {
 
     private var _binding: FragmentGuideDetailBinding? = null
     private val binding get() = _binding!!
     var result: String = ""
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        result = arguments?.getString("title")!!
         _binding = FragmentGuideDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.title.text = result
+        binding.title.text = arguments?.getString("Title")
     }
+
 
 }
