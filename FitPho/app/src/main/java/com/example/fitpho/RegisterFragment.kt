bfd4848c3ef75cecc59030b6b9801fa7450bf306
yunsuk990 <yunsuk990@naver.com>
2022-09-26 16:46:26 +0900

    Update RegisterFragment.kt

--- FitPho/app/src/main/java/com/example/fitpho/RegisterFragment.kt
+++ FitPho/app/src/main/java/com/example/fitpho/RegisterFragment.kt
@@ -9,9 +9,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.fitpho.Network.API
-import com.example.fitpho.NetworkModel.Register
-import com.example.fitpho.NetworkModel.RegisterResponse
-import com.example.fitpho.NetworkModel.getRetrofit
+import com.example.fitpho.NetworkModel.*
 import com.example.fitpho.databinding.FragmentRegisterBinding
 import com.example.fitpho.util.hideKeyboard
 import retrofit2.Call
@@ -25,7 +23,7 @@ class RegisterFragment : Fragment() {
     var userid: String = ""
     var userpasswd: String = ""
     var checkuserpasswd: String = ""
-    var nickname: String = ""
+    var confirm: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,51 +36,80 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnRegister.setOnClickListener {
-            userid = binding.registerUserid.text.toString()
-            userpasswd = binding.registerPassword.text.toString()
-            checkuserpasswd = binding.registerCheckpassword.text.toString()
-            nickname = binding.nickname.text.toString()
-            when {
-                userid.isEmpty() -> {
-                    Toast.makeText(requireContext(), "아이디를 입력하세요.", Toast.LENGTH_LONG).show()
+        binding.emailConfirm.setOnClickListener{
+            var useremail = binding.registerUserid.text.toString()
+            val authService = getRetrofit().create(API::class.java)
+            authService.emailConfirm(useremail).enqueue(object: Callback<EmailResponse> {
+                override fun onResponse(
+                    call: Call<EmailResponse>,
+                    response: Response<EmailResponse>
+                ) {
+                    if(response.isSuccessful){
+                        var post: EmailResponse? = response.body();
+                        Toast.makeText(requireContext(), post?.printMessage(), Toast.LENGTH_LONG).show()
+                        Log.d("Comfirm", "success")
+                        confirm = true
+                    }else{
+                        var post: EmailResponse? = response.body();
+                        Toast.makeText(requireContext(), post?.printMessage(), Toast.LENGTH_LONG).show()
+                        confirm = false
+                    }
                 }
-                userpasswd.isEmpty() -> {
-                    Toast.makeText(requireContext(), "비밀번호를 입력하세요.", Toast.LENGTH_LONG).show()
-                }
-                userpasswd != checkuserpasswd -> {
-                    Toast.makeText(requireContext(), "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
-                }
-                else -> {
-                    binding.progressBar2.visibility = View.VISIBLE
-                    val authService = getRetrofit().create(API::class.java)
-                    authService.registerIn(getRegister()).enqueue(object: Callback<RegisterResponse> {
-                        override fun onResponse(
-                            call: Call<RegisterResponse>,
-                            response: Response<RegisterResponse>,
-                        ) {
-                            if(response.isSuccessful){
-                                binding.progressBar2.visibility = View.GONE
-                                Log.d("REGISTERIN/SUCCESS", response.toString())
-                                hideKeyboard()
-                                findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
-                            }else{
-                                Log.d("REGISTERIN/FAILURE", "FAIL")
-                            }
-                        }
 
-                        override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
-                            Log.d("REGISTERIN/FAILURE", t.message.toString())
-                            binding.progressBar2.visibility = View.GONE
-                        }
-                    })
+                override fun onFailure(call: Call<EmailResponse>, t: Throwable) {
+                    confirm = false
+                }
+            })
+        }
+
+        binding.btnRegister.setOnClickListener {
+            if(confirm == true){
+                userid = binding.registerUserid.text.toString()
+                userpasswd = binding.registerPassword.text.toString()
+                checkuserpasswd = binding.registerCheckpassword.text.toString()
+
+                when {
+                    userid.isEmpty() -> {
+                        Toast.makeText(requireContext(), "이메일을 입력하세요.", Toast.LENGTH_LONG).show()
+                    }
+                    userpasswd.isEmpty() -> {
+                        Toast.makeText(requireContext(), "비밀번호를 입력하세요.", Toast.LENGTH_LONG).show()
+                    }
+                    userpasswd != checkuserpasswd -> {
+                        Toast.makeText(requireContext(), "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
+                    }
+                    else -> {
+                        binding.progressBar2.visibility = View.VISIBLE
+                        val authService = getRetrofit().create(API::class.java)
+                        authService.registerIn(getRegister()).enqueue(object: Callback<RegisterResponse> {
+                            override fun onResponse(
+                                call: Call<RegisterResponse>,
+                                response: Response<RegisterResponse>,
+                            ) {
+                                if(response.isSuccessful){
+                                    binding.progressBar2.visibility = View.GONE
+                                    Log.d("REGISTERIN/SUCCESS", "로그인 성공")
+                                    hideKeyboard()
+                                    confirm = false
+                                    findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+                                }else{
+                                    Log.d("REGISTERIN/FAILURE", "FAIL")
+                                }
+                            }
+
+                            override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+                                Log.d("REGISTERIN/FAILURE", t.message.toString())
+                                binding.progressBar2.visibility = View.GONE
+                            }
+                        })
+                    }
                 }
             }
         }
     }
 
     private fun getRegister(): Register{
-        return Register(userid, userpasswd, checkuserpasswd, nickname)
+        return Register(userid, userpasswd, checkuserpasswd)
     }
 
     override fun onDestroyView() {
