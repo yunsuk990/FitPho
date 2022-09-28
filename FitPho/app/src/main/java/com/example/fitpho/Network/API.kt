bfcfe8de82744e769fe9792f24de2d25ee58965b
yunsuk990 <yunsuk990@naver.com>
2022-09-28 11:07:13 +0900

    Update API.kt

--- FitPho/app/src/main/java/com/example/fitpho/Network/API.kt
+++ FitPho/app/src/main/java/com/example/fitpho/Network/API.kt
@@ -2,10 +2,7 @@ package com.example.fitpho.Network
 
 import com.example.fitpho.NetworkModel.*
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface API {
 
