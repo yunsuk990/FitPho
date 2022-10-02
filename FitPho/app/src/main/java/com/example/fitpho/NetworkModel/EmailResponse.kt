fa7b248e72438ac840ccc0838efa33762e953fb9
yunsuk990 <yunsuk990@naver.com>
2022-10-03 02:39:41 +0900

    Update EmailResponse.kt

--- FitPho/app/src/main/java/com/example/fitpho/NetworkModel/EmailResponse.kt
+++ FitPho/app/src/main/java/com/example/fitpho/NetworkModel/EmailResponse.kt
@@ -1,13 +1,14 @@
 package com.example.fitpho.NetworkModel
 
+//이메일 중복 확인 요청
 class EmailResponse (
-    val success: String,
-    val message: String
+    private val success: String,
+    private val message: String
     ){
+
     public fun printMessage(): String {
         return message
     }
-
     public fun printSuccess(): String{
         return success
     }
