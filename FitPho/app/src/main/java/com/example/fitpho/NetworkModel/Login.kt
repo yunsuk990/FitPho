854574a51d1dc4d7167a8b84028567badd3f6db1
yunsuk990 <yunsuk990@naver.com>
2022-10-03 02:39:34 +0900

    Update Login.kt

--- FitPho/app/src/main/java/com/example/fitpho/NetworkModel/Login.kt
+++ FitPho/app/src/main/java/com/example/fitpho/NetworkModel/Login.kt
@@ -2,6 +2,7 @@ package com.example.fitpho.NetworkModel
 
 import com.google.gson.annotations.SerializedName
 
+//로그인 요청
 data class Login(
     @SerializedName(value = "email") var email: String,
     @SerializedName(value = "password") var password: String
