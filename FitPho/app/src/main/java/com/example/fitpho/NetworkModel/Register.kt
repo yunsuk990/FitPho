1b1e97873574d6a87f9b565757b2545f0c665dbd
yunsuk990 <yunsuk990@naver.com>
2022-10-03 02:39:12 +0900

    Update Register.kt

--- FitPho/app/src/main/java/com/example/fitpho/NetworkModel/Register.kt
+++ FitPho/app/src/main/java/com/example/fitpho/NetworkModel/Register.kt
@@ -2,6 +2,7 @@ package com.example.fitpho.NetworkModel
 
 import com.google.gson.annotations.SerializedName
 
+//회원가입 요청
 data class Register(
     @SerializedName(value = "email") var email: String,
     @SerializedName(value = "password") var password: String,
